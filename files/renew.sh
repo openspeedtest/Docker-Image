2f66e6feb8d702b4cd3bfcfd11ac52d349f2dc56
OpenSpeedTest™ <51720450+openspeedtest@users.noreply.github.com>
2023-08-27 16:57:05 +0530

    Update renew.sh

--- files/renew.sh
+++ files/renew.sh
@@ -5,16 +5,16 @@ fullchain_path="/var/log/letsencrypt/live/${DOMAIN_NAME}/fullchain.pem"
 
 if [ "$ENABLE_LETSENCRYPT" = True ] && [ "$DOMAIN_NAME" ] && [ "$USER_EMAIL" ]; then
 
-certbot renew --force-renewal
+certbot certonly -n --webroot --webroot-path /usr/share/nginx/html --no-redirect --agree-tos --email "$USER_EMAIL" -d "$DOMAIN_NAME" --config-dir /var/log/letsencrypt/ --work-dir /var/log/letsencrypt/work --logs-dir /var/log/letsencrypt/log
     if [ $? -eq 0 ]; then
-        echo "certbot renew --force-renewal Executed."
+        echo "certbot certonly -n... Executed."
         if [ -f "$fullchain_path" ]; then
         nginx -s reload
         else
         echo "letsencrypt Certificates Not Found!"
         fi
     else
-        echo "certbot renew --force-renewal Failed."
+        echo "certbot certonly -n... Failed."
     fi
 
 fi
